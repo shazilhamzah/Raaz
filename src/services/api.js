153eb1c47da8e2a60df85b90710cc75c4e98b505
Shazil Hamzah <shazil.hamzah@outlook.com>
2026-01-31 17:45:22 +0500

    .env file setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { API_URL } from '@env';
 
 // REPLACE THIS with your computer's actual IP address
-const API_URL = 'https://raaz-api.onrender.com/api';
-// const API_URL = 'http://192.168.1.15:5000/api'; 
+// const API_URL = 'https://raaz-api.onrender.com/api';
+// // const API_URL = 'http://192.168.1.15:5000/api'; 
 
 const api = axios.create({
   baseURL: API_URL,
